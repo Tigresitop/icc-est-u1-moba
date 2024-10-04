b8d5086630c3d82adabc6e2d6acfdc8c911c10de
John Tigre <cuji309@gmail.com>
2024-10-04 18:56:39 -0500

    Se finalizo el proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        MetodoOrdenamiento 
+
+    public static void main(String[] args) {
+
+        // Crear una instancia de MetodoOrdenamiento
+        MetodoOrdenamiento metodo = new MetodoOrdenamiento();
+
+        // Crear el arreglo
+        int[] array = metodo.crearArray();
+
+        System.out.println();
+
+        // Ordenar el arreglo
+        metodo.sortBubbleMe(array);
+
+        System.out.println();
+
+        // Imprimir el arreglo ordenado
+        metodo.printArreglo(array);
     }
 }
--- src/MetodoOrdenamiento.java
+++ src/MetodoOrdenamiento.java
@@ -1,12 +1,79 @@
+import java.util.Scanner;
+
 public class MetodoOrdenamiento {
-    public int sortBubbleMe(int[] arreglo){
-        int m = arreglo.length;
-        for (int i = 0; i < m; i++) {
-            
+
+    // Método para crear el arreglo
+    public int[] crearArray() {
+        Scanner leer = new Scanner(System.in);
+        int tamanio = leerEnteroValido(leer, false); 
+
+
+        int[] arreglo = new int[tamanio];
+
+
+        System.out.println("Ingrese " + tamanio + " números enteros:");
+        for (int i = 0; i < tamanio; i++) {
+            arreglo[i] = leerEnteroValido(leer, true); 
         }
 
-
-
+        return arreglo;
     }
 
+    
+    public void sortBubbleMe(int[] arreglo) {
+        int m = arreglo.length;
+        boolean intercambio;
+        int aux;
+
+        for (int i = 0; i < m - 1; i++) {
+            intercambio = false;
+            System.out.println("Pasada " + (i + 1));
+
+            for (int j = 0; j < m - 1 - i; j++) {
+                System.out.println("Comparando arreglo[" + j + "]=" + arreglo[j] + " y arreglo[" + (j + 1) + "]=" + arreglo[j + 1]);
+
+                if (arreglo[j] > arreglo[j + 1]) {
+                    // Intercambio
+                    aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                    intercambio = true;
+
+                    System.out.println("Intercambio realizado: " + arreglo[j] + " con " + arreglo[j + 1]);
+                }
+            }
+
+            if (!intercambio) {
+                System.out.println("No hubo intercambios, el arreglo está ordenado.");
+                break;
+            }
+        }
+    }
+
+    // Método para imprimir el arreglo
+    public void printArreglo(int[] arreglo) {
+        System.out.println("Arreglo ordenado:");
+        for (int num : arreglo) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Método para leer enteros válidos
+    public static int leerEnteroValido(Scanner s, boolean numerosNegativos) {
+        int numero;
+        do {
+            System.out.println("Ingrese un número entero" + (numerosNegativos ? " (se permiten negativos):" : " positivo:"));
+            while (!s.hasNextInt()) {
+                System.out.println("Te solicité un Entero Válido");
+                s.next();
+            }
+            numero = s.nextInt();
+            if (!numerosNegativos && numero <= 0) {
+                System.out.println("El número debe ser positivo.");
+            }
+        } while (!numerosNegativos && numero <= 0);
+
+        return numero;
+    }
 }
